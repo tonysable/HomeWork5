98126949f5cea58b5eb799a260ffac8ceb43191d
Sable <bandidos_150@mail.ru>
2022-10-15 20:27:56 +0300

    Задача 36

---
+++ Task36/Program.cs
@@ -0,0 +1,27 @@
+﻿void Main()
+{
+    int[] array = new int[5]; 
+    Random abc = new Random(); 
+    for(int i = 0; i < 5; i++) 
+    {
+        array[i] = abc.Next(100, 1000);
+    }
+    Console.Write("["); 
+    for(int i = 0; i < 4; i++) 
+    {
+        Console.Write(array[i]); 
+        Console.Write(", ");
+
+    }
+    Console.Write(array[4]); 
+
+    
+    Console.Write("] => ");
+    int sum = 0; 
+    for(int i = 1; i < 5; i+=2) 
+    {
+        sum += array[i];    
+        }
+    Console.WriteLine(sum); 
+}
+Main();
